07abde7af645c8fadf49dcfc5f258b871c05634c
Carlos Granier <granier@gmail.com>
2022-01-26 10:54:57 -0500

    Ch. 21 Load Model

--- py21.py
+++ py21.py
@@ -946,6 +946,17 @@ class Model:
         with open(path, 'wb') as f:
             pickle.dump(model, f)
     
+    # Loads and returns a model
+    @staticmethod
+    def load(path):
+        
+        # Open file in binary-read mode, load a model
+        with open(path, 'rb') as f:
+            model = pickle.load(f)
+        
+        # Return a model
+        return model
+    
 # Common accuracy class
 class Accuracy:
     
@@ -1061,60 +1072,67 @@ y = y[keys]
 X = (X.reshape(X.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 X_test = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 
-# Instantiate the model
-model = Model()
-
-# Add layers
-model.add(Layer_Dense(X.shape[1], 128))
-model.add(Activation_ReLU())
-model.add(Layer_Dense(128,128))
-model.add(Activation_ReLU())
-model.add(Layer_Dense(128,10))
-model.add(Activation_Softmax())
-
-# Set loss, optimizer and accuracy objects
-model.set(
-    loss=Loss_CategoricalCrossentropy(),
-    optimizer=Optimizer_Adam(decay=1e-3),
-    accuracy=Accuracy_Categorical()
-)
-
-# Finalize the model
-model.finalize()
-
-# Train the model
-model.train(X, y, validation_data=(X_test, y_test), epochs=10, batch_size=128, print_every=100)
-
-# Save the parameters to a file
-model.save_parameters('fashion_mnist.parms')
-
-# Save the model to a file
-model.save('fashion_mnist.model')
-
-# New model
-
-# Instantiate the model
-model = Model()
-
-# Add layers
-model.add(Layer_Dense(X.shape[1], 128))
-model.add(Activation_ReLU())
-model.add(Layer_Dense(128, 128))
-model.add(Activation_ReLU())
-model.add(Layer_Dense(128, 10))
-model.add(Activation_Softmax())
-
-# Set loss and accuracy objects
-# We do not set optimizer object this time - there's no need to do it
-# as we won't train the model
-model.set(loss=Loss_CategoricalCrossentropy(), accuracy=Accuracy_Categorical())
-
-# Finalize the model
-model.finalize()
-
-# Set model with parameters instead of training it
-model.load_parameters('fashion_mnist.parms')
+# Load the model
+model = Model.load('fashion_mnist.model')
 
 # Evaluate the model
 model.evaluate(X_test, y_test)
+
+
+# # Instantiate the model
+# model = Model()
+
+# # Add layers
+# model.add(Layer_Dense(X.shape[1], 128))
+# model.add(Activation_ReLU())
+# model.add(Layer_Dense(128,128))
+# model.add(Activation_ReLU())
+# model.add(Layer_Dense(128,10))
+# model.add(Activation_Softmax())
+
+# # Set loss, optimizer and accuracy objects
+# model.set(
+#     loss=Loss_CategoricalCrossentropy(),
+#     optimizer=Optimizer_Adam(decay=1e-3),
+#     accuracy=Accuracy_Categorical()
+# )
+
+# # Finalize the model
+# model.finalize()
+
+# # Train the model
+# model.train(X, y, validation_data=(X_test, y_test), epochs=10, batch_size=128, print_every=100)
+
+# # Save the parameters to a file
+# model.save_parameters('fashion_mnist.parms')
+
+# # Save the model to a file
+# model.save('fashion_mnist.model')
+
+# # New model
+
+# # Instantiate the model
+# model = Model()
+
+# # Add layers
+# model.add(Layer_Dense(X.shape[1], 128))
+# model.add(Activation_ReLU())
+# model.add(Layer_Dense(128, 128))
+# model.add(Activation_ReLU())
+# model.add(Layer_Dense(128, 10))
+# model.add(Activation_Softmax())
+
+# # Set loss and accuracy objects
+# # We do not set optimizer object this time - there's no need to do it
+# # as we won't train the model
+# model.set(loss=Loss_CategoricalCrossentropy(), accuracy=Accuracy_Categorical())
+
+# # Finalize the model
+# model.finalize()
+
+# # Set model with parameters instead of training it
+# model.load_parameters('fashion_mnist.parms')
+
+# # Evaluate the model
+# model.evaluate(X_test, y_test)
 
